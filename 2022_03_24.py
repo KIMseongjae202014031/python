ada6f88630723943e66dba6edd6c2e224c694c6a
KIMseongjae202014031 <101773416+KIMseongjae202014031@users.noreply.github.com>
2022-03-24 17:54:45 +0900

    Add files via upload

---
+++ 2022_03_24.py
@@ -0,0 +1,44 @@
+#lang = 'python'            #1번
+#print(lang[0], lang[2])
+
+#string = "PYTHON"      #2번
+#print(string[::-1])
+
+#url = input("http://sharebook.kr")     #3번
+#print(url)
+
+#file_name = "보고서.xlsx"      #4번
+#print(file_name.endswith('xlsx'))
+
+#file_name = "2020_보고서.xlsx"      #5번
+#print(file_name.startswith('2020'))
+
+#score = int(input("학점을 입력해 주세요"))     #6번
+# if 81 < score < 100 :
+#    print('A')
+# elif 61 < score <80 :
+#    print('B')
+# elif 41 < score <60 :
+#    print('C')
+# elif 21 < score <40 :
+#    print('D')
+# elif 0 < score <20 :
+#     print('E')
+
+# cook = ["피자", "깁밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]       #7번
+# print(len(cook))
+
+#list = input("투자 경고 프로그램 입니다. 투자종목을 말해주세요") #8번
+#if list in["Microsoft","Google","Naver","Kakao","SAMSUNG",'LG'] :
+#    print('투자 경고! ')
+#else :
+#    print('투자 가능 ')
+
+#list = [100,200,300]       #9번
+#for i in list:
+#    print(i+10)
+
+#list = ["SK하이닉스", "삼성전자", "LG전자"]        #10번
+#print(len(list[0]))
+#print(len(list[1]))
+#print(len(list[2]))
